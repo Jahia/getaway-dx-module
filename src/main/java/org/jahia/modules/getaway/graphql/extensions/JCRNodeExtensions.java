3c758d649709594d301eb4f6207b859f041339e1
vandre <vandre@jahia.com>
2018-04-19 14:15:27 +0200

    Add ability to request node's url / Add headerPhoto field to gant:destination

--- src/main/java/org/jahia/modules/getaway/graphql/extensions/JCRNodeExtensions.java
+++ src/main/java/org/jahia/modules/getaway/graphql/extensions/JCRNodeExtensions.java
@@ -1,10 +1,13 @@
 package org.jahia.modules.getaway.graphql.extensions;
 
+import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLTypeExtension;
 import org.apache.commons.lang.StringUtils;
 import org.jahia.modules.graphql.provider.dxm.node.GqlJcrNode;
+import org.jahia.modules.graphql.provider.dxm.node.GqlJcrProperty;
+import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.utils.LanguageCodeConverters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +27,7 @@ public class JCRNodeExtensions {
     }
 
     @GraphQLField
+    @GraphQLDescription("The displayable country name")
     public String country(@GraphQLName("propertyName") String pName, @GraphQLName("locale") String localeCode) {
         final String propertyName = StringUtils.isBlank(pName) ? "country" : pName;
         final Locale locale = StringUtils.isBlank(localeCode) ? Locale.ENGLISH : LanguageCodeConverters.getLocaleFromCode(localeCode);
@@ -32,4 +36,13 @@ public class JCRNodeExtensions {
         return new Locale("en", isoCode).getDisplayCountry(locale);
     }
 
+    /**
+     *  Retrieves the relative Url of the node
+     * @return the relative Url of the node
+     */
+    @GraphQLField
+    @GraphQLDescription("The value of the node's url")
+    public String getNodeUrl() {
+        return node.getNode().getUrl();
+    }
 }
